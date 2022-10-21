51eccc10d29ed237197e97cde8bf3ac0334c85f8
Ivan Moscoso <moscosoi2002@gmail.com>
2022-10-21 19:12:55 +0200

    7.1 ruta + controlador

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,9 +14,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/', function (Request $request) {
+    $message = 'Loading welcome page';
+    Log::info($message);
+    $request->session()->flash('info', $message);
     return view('welcome');
-});
+ });
+ 
 
 Route::get('/dashboard', function () {
     return view('dashboard');

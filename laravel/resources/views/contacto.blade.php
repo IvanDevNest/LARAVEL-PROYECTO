d3851650d2d7751b714eb04b87b69dde53ba42dc
Arnau Olea <arnauolea@gmail.com>
2023-02-16 16:35:42 +0100

    m9

--- laravel/resources/views/contacto.blade.php
+++ laravel/resources/views/contacto.blade.php
@@ -4,10 +4,18 @@
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css"
      integrity="sha256-kLaT2GOSpHechhsozzB+flnD+zUyjE2LlfWPgU04xyI="
      crossorigin=""/>
+	
+	<link rel="stylesheet" href="http://cdn.leafletjs.com/leaflet/v0.7.7/leaflet.css" />
+		
     <script src="https://unpkg.com/leaflet@1.9.3/dist/leaflet.js"
      integrity="sha256-WBkoXOwTeyKclOHuWtc+i2uENFpDZ9YPdf5Hf+D7ewM="
      crossorigin=""></script>
+	 
 
+	<script src="recursos/js/leaflet.js"></script>
+	<script src="recursos/js/L.Control.Locate.min.js"></script>
+	<script src="https://unpkg.com/togeojson@0.14.2"></script>
+	<script>src="https://raw.githubusercontent.com/dmauro/Keypress/2.1.5/keypress-2.1.5.min.js"</script>
 
 </head>
 <style>
@@ -134,16 +142,16 @@ h1 {
 			<div class="content">
 				<h1>Contactanos!</h1>
 				<h3>Envia tu mensaje</h3>
-				<a href="https://pornhub.com" class="btn">Formulario de contacto</a>
+				<a href="https://youtube.com" class="btn">Formulario de contacto</a>
 			</div>
+			<a href="https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwjNu5TuoYv9AhUXPuwKHdJeCIAQFnoECAsQAQ&url=https%3A%2F%2Fdocs.google.com%2Fforms%2Fcreate%3Fhl%3DES&usg=AOvVaw3lqGk_68o6EJPs6z_c6Z0_" accesskey="h"></a><br>
 </div>
     </header>
 	<main>
         <div id="about">
-            <h1>Quieres Visitarnos?</h1>
-			<p>Encuentranos en esta direccion!</p>
-            <br>
-            <div id="map"></div>
+		<h1 class="body-h1">Vols visitar-nos?</h1>
+    <a href="#" onClick="getLocation()"><h3 class="body-h3">Ubica'ns al mapa</h3></a>
+    <div id="map"></div>
            
         </div>
     </main>	
@@ -159,38 +167,38 @@ h1 {
             </div>
         </div>
     </footer>
-    <script>
-        window.addEventListener('load',function(){
-            x = navigator.geolocation.getCurrentPosition(showPosition);
-        })
-        //Funciones para tener
-       var map = L.map('map').setView([41.231273815596715, 1.7282039997421028], 13);
+	<script>
 
-        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        }).addTo(map);
+var map = L.map('map').setView([41.23114477320315, 1.7281181849031044], 18);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+maxZoom: 18,
+attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+var marker = L.marker([41.23114477320315, 1.7281181849031044]).addTo(map);
 
-        L.marker([41.231273815596715, 1.7282039997421028]).addTo(map)
-            .bindPopup('Estamos en esta ubicacion!')
-            .openPopup();
-        
+L.Routing.control({
+waypoints: [
+	L.latLng(41.23114477320315, 1.7281181849031044),
+	L.latLng(41.22317016641275, 1.7176998961503782)
+]
+}).addTo(map);
 
-            // Funciones ubicacion del usuario
-            
+function getLocation(e) {
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(showPosition);
+	} else { 
+		x.innerHTML = "Geolocation is not supported by this browser.";
+	}
+	e.preventDefault()
+	return false
+}
 
-            function showPosition(position) {
-            x.innerHTML = "Latitude: " + position.coords.latitude + 
-            "<br>Longitude: " + position.coords.longitude;
-            }
-
-            
-            var popup = L.popup()
-                .setLatLng([position.coords.latitude, position.coords.longitude])
-                .setContent("Usted esta aqui.")
-                .openOn(map);
+function showPosition(position) {
+	var marker = L.marker([position.coords.latitude, position.coords.longitude]).addTo(map);
+	var circle = L.circle([position.coords.latitude, position.coords.longitude],{color:'blue', fillColor: '#0000FF', fillOpacity: 0.5, radius: 30}).addTo(map);
+}
 
 
-    
-            </script>
+</script>
 
 @endsection
